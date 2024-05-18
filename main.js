10053df2a7751cdb692592b2e75ac6a1471953f2
Michael <114317937+thatonecodes@users.noreply.github.com>
2024-05-18 01:41:40 -0400

    fix styling, remove dead code

--- main.js
+++ main.js
@@ -1,35 +1,13 @@
 import './style.css'
-import javascriptLogo from '/javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 import { generateMentalHealthTip } from "./mentalhealth.js"
 
-const otherDiv = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button">
-      </button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-document.querySelector('#app').innerHTML = otherDiv;
 
 //add interscectionobserver for main page
 const observer = new IntersectionObserver(
   entries => {
     entries.forEach((entry) => {
       console.log(entry);
+      entry.target.classList.toggle("show", entry.isIntersecting);
     });
   },
 );
@@ -154,14 +132,9 @@ function createNewsElements(apiUrl){
         newsContent.appendChild(nextBackDiv);
       })
     .catch((err) => {
-        console.error(err);
-      });
+      console.error(err);
+
+    });
 }
 
 console.log(generateMentalHealthTip());
-setupCounter(document.querySelector('#counter'))
-
-
-
-
-
